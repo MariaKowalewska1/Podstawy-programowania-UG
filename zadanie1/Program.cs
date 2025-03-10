e902a9c69769d113a8fee342611d430c09ef9f6e
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-03-10 21:58:27 +0100

    zadania 1,2

---
+++ zadanie1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to liczba parzysta, czy też nieparzysta
+            Double a;
+            Console.WriteLine("Podaj liczbe: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            if (a % 2.0 == 0)
+                Console.WriteLine("Liczba jest parzysta");
+            else
+                Console.WriteLine("Liczba jest nieparztsta");
+
+
+
+        }
+    }
+}
---
+++ zadanie2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący liczbę całkowitą i wypisujący informację, czy wczytana liczba jest dodatnia, ujemna, czy też równa zero
+            Double a;
+            Console.WriteLine("Podaj liczbe: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            if (a > 0)
+                Console.WriteLine("Liczba jest dodatnia");
+            else if (a < 0)
+                Console.WriteLine("Liczba jet ujemna");
+            else
+                Console.WriteLine("liczba jest rowna 0");
+        }
+    }
+}
