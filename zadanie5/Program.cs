0fdc7136459702e54c54f6c73b4ea79c072037d1
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-03-17 16:03:37 +0100

    zadania 5,6,7

---
+++ zadanie5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący rok i wypisujący na ekranie informację czy jest to rok przestępny czy też nie
+            Double rok;
+            Console.WriteLine("Podaj rok: ");
+            rok = Convert.ToDouble(Console.ReadLine());
+            if (((rok % 4 == 0) && (rok % 100 != 0)) || (rok % 400 == 0))
+                Console.WriteLine("Rok {0} jest przestępny", rok);
+            else
+                Console.WriteLine("Rok {0} nie jest przestepny", rok);
+
+                
+        }
+    }
+}
