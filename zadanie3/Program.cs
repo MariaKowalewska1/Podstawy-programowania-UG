f38921173af34ee40e32a6e0cd14e9fc9197cbdb
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-03-17 14:26:26 +0100

    zadanie 3,4

---
+++ zadanie3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący trzy liczby i wypisujący na ekranie wartość najmniejszej i największej z tych liczb
+            Double a, b, c;
+            Console.WriteLine("Podaj liczbe a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            if ((a > b) && (a > c))
+                Console.Write("Licbza a jest największa ");
+            else if ((b > a) && (b >c))
+                Console.Write("Liczba b jest najwieksza ");
+            else
+                Console.Write("Liczba c jest najwieksza ");
+            if ((a < b) && (a < c))
+                Console.Write("Liczba a jest najmniejsza ");
+            else if ((b < a) && (b < c))
+                Console.Write("Liczba b jest najmniejsza ");
+            else
+                Console.Write("Liczba c jest najmniejsza ");
+           
+
+
+                
+            
+
+        }
+    }
+}
---
+++ zadanie4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący pięć liczb i wypisujący na ekranie wartość najmniejszej i największej z tych liczb
+            Double a, b, c,d,e,min,max;
+            Console.WriteLine("Podaj liczbe a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe c: ");
+            d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe c: ");
+            e = Convert.ToDouble(Console.ReadLine());
+            min = a;
+            if (b<min) min=b;
+            if (c<min) min=c;
+            if (d<min) min=d;
+            if (e < min) min = e;
+            Console.WriteLine("Liczba {0} jest najmniejsza", min);
+            max = a;
+            if (b > max) max = b;
+            if (c > max) max = c;
+            if (d > max) max = d;
+            if (e > max) max = e;
+            Console.WriteLine("Liczba {0} jest najwieksza", max);
+
+
+
+
+
+
+
+        }
+    }
+}
