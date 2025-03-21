ec88e5b012ed591edbd5c52da842ac6ef5f69682
Anna Nowak <nowak.anna2006@gmail.com>
2025-03-21 14:14:55 +0100

    zadania 4-7

---
+++ zadanie.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wczytaj dwie liczby naturalne dodatnie n i k. Narysuj z gwiazdek na ekranie prostokąt posiadający n wierszy i k kolumn. Użyj pętli for
+            Double n, k;
+            Console.WriteLine("Podaj liczbe naturalna n: ");
+            n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe naturalna k: ");
+            k = Convert.ToDouble(Console.ReadLine());
+            int i, j;
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= k; j++) 
+                Console.Write("*");
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ zadanie.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Wczytaj liczbę naturalną n (n>1). Narysuj na ekranie z gwiazdek trójkąt prostokątny. Użyj pętli for. Przykładowo dla n = 3 narysowany trójkąt powinien wyglądać:
+            *
+            ** 
+            ***/
+            double n;
+            Console.WriteLine("Podaj liczbe naturalna n wieksza od 1: ");
+            n = Convert.ToDouble(Console.ReadLine());
+            int i, j;
+            for (i = 1; i <= n; i++) 
+            {
+                for (j = 1; j<= i; j++)
+                    Console.Write("*");
+                Console.WriteLine("");
+            }
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
