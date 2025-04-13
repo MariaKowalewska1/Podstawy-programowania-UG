2b7d11862ee3ba8fde9bbf296fb6cfd36a228fef
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-04-13 20:21:17 +0200

    .

---
+++ zadanie-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*Napisać program wczytujący współrzędne punktu P=(x,y) i wypisujący na ekranie informację, 
+do której ćwiartki ten punkt należy, czy też informację, że leży on na osi OX i/lub osi OY.*/
+
+            int x, y;
+            Console.Write("Podaj punkt x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj punkt y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            if ((x > 0) && (y > 0)) Console.WriteLine("Punkt({0},{1}) nalezy do 1 cwiartki", x, y);
+            else if ((x < 0) && (y > 0)) Console.WriteLine("Punkt ({0},{1}) nalezy do 2 cwiartki", x, y);
+            else if ((x < 0) && (y < 0)) Console.WriteLine("Punkt({0},{1}) nalezy do 3 cwiartki", x, y);
+            else if ((x > 0) && (y < 0)) Console.WriteLine("Punkt ({0},{1}) nalezy do 4 cwiartki", x, y);
+            if (y == 0) Console.WriteLine("Punkt znajduje sie na osi OX");
+            else if (x == 0) Console.WriteLine("Punkt znajduje sie na osi OY");
+
+
+        }
+    }
+}
---
+++ zadanie-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*Napisać program wczytujący współrzędne dwóch punktów A=(x1,y1) i B=(x2,y2) oraz 
+wypisujący na ekranie wzór funkcji liniowej przechodzącej przez te dwa punkty. 
+Wskazówka: Równanie prostej przechodzącej przez dwa punkty: */
+
+
+            
+        }
+    }
+}
