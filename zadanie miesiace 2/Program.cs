ac9bb316f30dc4da6338cdd03bc1debdb75031d3
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-05-19 19:44:28 +0200

    zadania miesiace

---
+++ zadanie miesiace 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_miesiace_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] miesiace = new string[] { "styczen", "luty", "marzec", "kwiecien","maj","czerwiec", "lipiec", "sierpien", "wrzesien", "pazdziernik", "listopad", "grudzien" };
+            int i;
+            for (i=0;i<miesiace.Length;i++)
+            {
+                Console.WriteLine(miesiace[i]);
+            }
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ zadanie-miesiace/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_miesiace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//zadeklaruj tablice z nazwami miesiecy
+            string[] miesiace = new string[] { "styczen", "luty", "marzec", "kwiecien", "maj", "czerwiec", "lipiec", "sierpien", "wrzesien", "pazdziernik", "listopad", "grudzien" };
+            Console.Write(string.Join(", ", miesiace));
+            Console.ReadKey(true);
+        }
+    }
+}
