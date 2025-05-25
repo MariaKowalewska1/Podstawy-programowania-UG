260ecda4480fc83c72b0af92e1835742be43d9b3
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-05-25 16:12:03 +0200

    niedokonczone zad.kolo

---
+++ zad-kolo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_kolo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//napisz metode ktora pokazuje wynik odejmowania wartosci wskazanej komorki dwoch tablic dwuwymiarowych
+
+
+          int[,] tab1 = new int[2, 3] {//najpierw zadeklaruj rozmiar a potem wypisuj wyrazy
+                { 1,2,3 },
+                { 4,5,6 }
+            };
+            int[,] tab2 = new int[2, 3]
+            {
+                { 7, 8, 9 },
+                { 10, 11, 12 }
+            };
+            Console.Write("Podaj indeks wiersza z pierwszej tablicy: ");
+            int i1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj indeks kolumny z pierwszej tablicy: ");
+            int j1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj indeks wiersza z drugiej tablicy: ");
+            int i2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj indeks kolumny z drugiej tablicy: ");
+            int j2 = Convert.ToInt32(Console.ReadLine());
+
+           
+            }
+        }
+
+    } 
---
+++ zad5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad5
+{
+    internal class Program
+    {
+        static int czy_pierwsza(int liczba)
+        {//Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja 
+         //powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb.Przetestuj działanie tworzonej funkcji
+            int i;
+            for (i = 2; i < liczba / 2; i++)
+                if (liczba % i == 0) return 0;
+            return 1;
+        }
+        static void Main(string[]args)
+        {
+            Console.WriteLine("czy 10 jest liczba pierwsza: {0}", czy_pierwsza(10));
+            Console.WriteLine("czy 2 jest liczba pierwsza: {0}", czy_pierwsza(2));
+        }
+    }
+}
