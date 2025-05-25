dbd04fd30a1e36dc744eb9ac4a4b99ed13bffb1b
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-05-25 17:30:40 +0200

    zadania kolo wszystkie 3

---
+++ zad-kolo2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_kolo2
+{
+    internal class Program
+    {
+        static void odwrotnosc(int[]tab)
+        {//napisz metode ktora pokazuje zawartosc tablicy jednowymiarowej w odwrotnej kolejnosci
+            for (int i = tab.Length-1;i>=0;i--)
+                Console.Write(tab[i]);
+        }
+        static void Main(string[] args)
+        {
+            int[] tablica = { 1, 4, 7, 8 };
+            Console.Write("Elementy tablicy odwrotnie: ");
+            odwrotnosc(tablica);
+                }
+    }
+}
---
+++ zad-kolo3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_kolo3
+{
+    internal class Program
+    {
+        static void scalenie(int[] tab1, int[] tab2)
+        {//napisz metode ktora zwraca dwie scalone tablice jednowymiarowe
+            foreach (int x in tab1)
+                Console.Write("{0}, ", x);
+            foreach (int x in tab2)
+                Console.Write("{0}, ", x);
+        }
+        static void Main(string[] args)
+        {
+            int[] tablica1 = { 1, 2, 3, 4 };
+            int[] tablica2 = { 5, 6, 7, 8 };
+            Console.Write("Elementy obu tablic: ");
+            scalenie(tablica1, tablica2);
+
+        }
+    }
+}
