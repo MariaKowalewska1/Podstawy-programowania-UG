9a1e53ab9799d764d6ee08b51366efbcb8e09b60
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-06-20 16:01:11 +0200

    zad 1 i 2

---
+++ zadan1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+namespace zadan1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i, n, suma = 0;
+            Console.Write("Podaj n=");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++) suma += i;
+
+            string folder = @"c:\tmp";
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            // zapis do pliku
+            string nazwa = @"c:\tmp\wynik1.txt";
+            FileStream plik = new FileStream(nazwa, FileMode.Create, FileAccess.Write, FileShare.None);
+            StreamWriter zapis = new StreamWriter(plik);
+            zapis.WriteLine(suma);
+            zapis.Close();
+            plik.Close();
+
+            Console.ReadKey(true); 
+        }
+    }
+}
+        
