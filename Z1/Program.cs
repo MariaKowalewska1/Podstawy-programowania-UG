3e99e14ebb3fc7aa43b86963432c9d25c21724aa
MariaKowalewska1 <m.kowalewska.045@studms.ug.edu.pl>
2025-06-20 12:17:28 +0200

    rekurencja

---
+++ Z1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z1
+{
+    internal class Program
+    {
+        static long silnia(int n)
+        {
+            if (n == 0) return 1;
+            else return n * silnia(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Błędny argument!");
+            else Console.WriteLine("{0}!={1}", n, silnia(n));
+            Console.ReadKey(true);
+        }
+    }
+}
